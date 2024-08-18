d4da999b2efe8b5ec274d557ab05fff0fdd97495
qet1717 <qet1717@yahoo.com.tw>
2024-08-19 06:24:37 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/weakhomework2/',
+  base: '/ weakhomework2/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
